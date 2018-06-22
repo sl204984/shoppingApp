b1048457319e14e9f95b915b8696477095f0a3a3
sl204984 <17798520003@163.com>
2018-06-22 14:07:00 +0800

    消息列表

--- src/utils/message-item/index.js
+++ src/utils/message-item/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 import { gray, colorhot, lightGray, baseColor } from '../common-styles';
 
-export default class MessageItem extends Comment {
+const { width } = Dimensions.get('window');
+
+export default class MessageItem extends Component {
   constructor(props) {
     super(props);
   }
@@ -14,20 +16,48 @@ export default class MessageItem extends Comment {
       chartInfo,
       shoppingImg
     } = this.props;
-    return <TouchableOpacity onPress={() => {}} style={styles.itemContainer}>
-
-    
-    </TouchableOpacity>
+    return <View style={styles.itemContainer}>
+      <Image style={styles.avatar} source={avator} />
+      <View style={styles.messageBox}>
+        <Text style={styles.publisher}>{ publisher.substr(0, 16) }</Text>
+        <Text style={styles.chartInfo}>{ chartInfo.substr(0, 20) }</Text>
+      </View>
+      <Image style={styles.shoppingImg} source={shoppingImg} />
+    </View>
   }
 }
 
 const styles = StyleSheet.create({
   itemContainer: {
-    marginBottom: 20,
+    width: width,
     backgroundColor: 'white',
     paddingTop: 10,
     paddingLeft: 10,
     paddingRight: 10,
-    height: 150
+    height: 100,
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: lightGray
   },
+  avatar: {
+    width: 60,
+    height: 60
+  },
+  messageBox: {
+    flex: 1,
+    paddingTop: 5,
+    paddingRight: 10,
+    paddingBottom: 5,
+    paddingLeft: 10
+  },
+  publisher: {
+    marginBottom: 10,
+  },
+  chartInfo: {
+    color: gray
+  },
+  shoppingImg: {
+    width: 80,
+    height: 80
+  }
 })
