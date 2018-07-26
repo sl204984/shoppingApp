5242a574e3017eba28c2c2885f0b3b8d68090178
sl204984 <17798520003@163.com>
2018-07-26 17:36:12 +0800

    配置

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,16 @@
-export default {
-  HOST: "http://localhost:3000/",
-  // HOST: "http://sl204984.com/",
-  IMG_HOST: "http://localhost:3000/",
-  // IMG_HOST: "http://sl204984.com/",
-  HTTP_TIME_OUT: 5,
+let CONFIG;
+if (__DEV__) {
+  CONFIG = {
+    HOST: "http://localhost:3000/",
+    IMG_HOST: "http://localhost:3000/",
+    HTTP_TIME_OUT: 5,
+  }
+} else {
+  CONFIG = {
+    HOST: "http://sl204984.com/",
+    IMG_HOST: "http://sl204984.com/",
+    HTTP_TIME_OUT: 5,
+  }
 }
+
+export default CONFIG;
