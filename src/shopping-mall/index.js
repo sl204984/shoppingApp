6e4c5416584e8eb139719dce9a98aa475b2bc863
sl204984 <17798520003@163.com>
2018-08-04 14:36:41 +0800

    修改样式

--- src/shopping-mall/index.js
+++ src/shopping-mall/index.js
@@ -1,58 +1,126 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { container } from "../utils/common-styles";
-import ImagePicker from "react-native-image-picker";
+import React, { Component } from "react";
+import { View, FlatList, ActivityIndicator, Text, Image } from 'react-native';
+import { connect } from 'react-redux'; // 引入connect函数
 
-const options = {
-  title: 'Select Avatar',
-  customButtons: [
-    {name: 'fb', title: 'Choose Photo from Facebook'},
-  ],
-  storageOptions: {
-    skipBackup: true,
-    path: 'images'
-  }
-};
+import ShoppingItem from '../utils/shopping-item';
+import { homePageActions, loginActions } from '../../actions';
+import SearchBox from './search-box';
+import { indexStyles as styles } from './styles';
 
-export default class Category extends Component {
-
+class HomePage extends Component {
   state = {
-    avatarSource: 'avatarSource'
+    refreshing: false,
+    loadingStatus: false,
+    pageNum: 0,
+    pageSize: 10,
+    curTabIndex: 0
+  }
+
+  componentDidMount() {
+    this._initDataList();
+    const { loginInfo } = this.props;
+    
   }
 
   render() {
-    return <View style={styles.container}>
-      <Text onPress={this._selectImg}>Here category is!</Text>
-    </View>
+    const { data = [], navigation, end } = this.props;
+    const { refreshing, curTabIndex } = this.state;
+    return (
+      <View style={styles.container}>
+        <SearchBox navigation={navigation} />
+        
+        <FlatList 
+          data={data} 
+          ListEmptyComponent={this._renderEmpty}
+          // ListHeaderComponent={this._renderHeader}
+          renderItem={this._renderItem}
+          ListFooterComponent={this._renderFooter}
+          onRefresh={this._initDataList}
+          refreshing={refreshing}
+          onEndReached={data.length < 100 && !end && this._addDataList}
+          // onEndReachedThreshold={0.1}
+        />
+      </View>
+    )
   }
 
-  _selectImg = () => {
-    ImagePicker.showImagePicker(options, (response) => {
-      console.log('Response = ', response);
-    
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      }
-      else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      }
-      else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      }
-      else {
-        let source = { uri: response.uri };
-    
-        // You can also display the image using data:
-        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-    
-        this.setState({
-          avatarSource: source
-        });
-      }
-    });
+  _renderItem = ({ item }) => (
+    <ShoppingItem avatar={item.avatar} 
+      price={item.price} 
+      shoppingName={item.shoppingName || []} 
+      publisher={item.publisher} 
+      imgList={item.imgList}
+      point={item.point} 
+      location={item.location || ''} />
+  )
+
+  _renderEmpty = () => {
+    const { refreshing } = this.state;
+    if(refreshing) return null;
+    return (
+      <View style={styles.emptyBox}>
+        <Text style={styles.textGray}>一个商家也没有~</Text>
+        <Image source={require('../local-imgs/no-merchant.png')} />
+      </View>
+    )
+  }
+
+  _renderHeader = () => {
+    const { refreshing, loadingStatus } = this.state;
+    if(refreshing || loadingStatus) return null;
+    return (
+      <View />
+    )
+  }
+
+  _renderFooter = () => {
+    const { data = [] } = this.props;
+    const { refreshing, loadingStatus } = this.state;
+    if(refreshing || data.length === 0) return null;
+    if(loadingStatus) { // 加载等待页
+      return (
+        <View style={styles.loadingBox}>
+          <ActivityIndicator animating={true} />
+        </View>
+      )
+    }
+    return (
+      <View style={styles.footerBox}>
+        <View style={styles.footerMidLine} />
+        <Text style={styles.footerText}>其实我也是有底线的</Text>
+        <View style={styles.footerMidLine} />
+      </View>
+    )
+  }
+
+  _initDataList = async () => {
+    const { initDataList } = this.props;
+    const { refreshing, pageSize, curTabIndex } = this.state;
+    if(refreshing) return;
+    this.setState({ refreshing: true });
+    await initDataList({ pageSize, pageNum: 0,  });
+    this.setState({ refreshing: false, pageNum: 1, type: curTabIndex });
+  }
+
+  _addDataList = async () => {
+    const { addDataList } = this.props;
+    const { loadingStatus, pageSize, pageNum, curTabIndex } = this.state;
+    if(loadingStatus) return;
+    this.setState({ loadingStatus: true });
+    await addDataList({ pageSize, pageNum });
+    this.setState({ loadingStatus: false, pageNum: pageNum + 1, type: curTabIndex });
   }
 }
 
-const styles = StyleSheet.create({
-  container: container.toJS()
-});
+export default connect(
+  (state) => ({
+    data: state.homePageReducer.data,
+    end: state.homePageReducer.end,
+    loginInfo: state.loginReducer.loginInfo
+  }),
+  dispatch => ({
+    initDataList: params => dispatch(homePageActions.initDataList(params)),
+    addDataList: params => dispatch(homePageActions.addDataList(params)),
+    fetchLoginInfo: () => dispatch(loginActions.initLoginInfo())
+  })
+)(HomePage);
