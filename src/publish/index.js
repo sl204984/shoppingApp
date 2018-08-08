8499b790af091546276b968c106ba4949fd8ea63
sl204984 <17798520003@163.com>
2018-08-09 00:47:18 +0800

    滚动

--- src/publish/index.js
+++ src/publish/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { 
+import {
+  ScrollView, 
   View, 
   StyleSheet, 
   Text, 
@@ -53,74 +54,76 @@ export default class PublishDemo extends Component {
       type
     } = this.state;
     return (
-      <View style={styles.container}>
-        <HeaderTitle 
-          value={shoppingName}
-          changeValue={val => {
-            this.setState({ shoppingName: val });
-          }}
-        />
+      <ScrollView>
+        <View style={styles.container}>
+          <HeaderTitle 
+            value={shoppingName}
+            changeValue={val => {
+              this.setState({ shoppingName: val });
+            }}
+          />
 
-        <Detail
-          value={desc}
-          changeValue={val => {
-            this.setState({ desc: val });
-          }} />
+          <Detail
+            value={desc}
+            changeValue={val => {
+              this.setState({ desc: val });
+            }} />
 
-        <ImgsDemo 
-          imgList={imgList} 
-          changeImgs={imgList => this.setState({ imgList })} 
-          maxFiles={maxFiles}
-        />
+          <ImgsDemo 
+            imgList={imgList} 
+            changeImgs={imgList => this.setState({ imgList })} 
+            maxFiles={maxFiles}
+          />
 
-        <Price 
-          price={price} 
-          shipFee={shipFee}
-          changePrice={({ price, shipFee }) => this.setState({ 
-            price, shipFee 
-          })} 
-        />
+          <Price 
+            price={price} 
+            shipFee={shipFee}
+            changePrice={({ price, shipFee }) => this.setState({ 
+              price, shipFee 
+            })} 
+          />
 
-        <Count
-          count={count}
-          changeCount={({ count }) => this.setState({ count })} 
-        />
+          <Count
+            count={count}
+            changeCount={({ count }) => this.setState({ count })} 
+          />
 
-        <Classification 
-          value={type} 
-          changeType={({ type }) => this.setState({ type })} 
-        />
+          <Classification 
+            value={type} 
+            changeType={({ type }) => this.setState({ type })} 
+          />
 
-        <Store 
-          store={store} 
-          changeStore={({ store }) => this.setState({ store })}
-        />
-        <Location />
+          <Store 
+            store={store} 
+            changeStore={({ store }) => this.setState({ store })}
+          />
+          <Location />
 
-        <TouchableOpacity style={styles.loginBox} onPress={ async () => {
-          if (imgList.length === 0) {
-            Toast.show('请选择商品图片~', {
-              position: Toast.positions.CENTER
-            });
-            return;
-          } else if (imgList.length > maxFiles) {
-            Toast.show(`最多可上传${maxFiles}张图片`, {
-              position: Toast.positions.CENTER
-            });
-            return;
-          }
-          const files = imgList.map((item, index) => {
-            const imgType = item.mime && item.mime.split('/')[1];
-            return {
-              path: item.path,
-              filename: 'sl204984_' + index + '.' + (imgType || 'jpeg') // 命名规则：订单号 + 索引 + 文件类型
+          <TouchableOpacity style={styles.loginBox} onPress={ async () => {
+            if (imgList.length === 0) {
+              Toast.show('请选择商品图片~', {
+                position: Toast.positions.CENTER
+              });
+              return;
+            } else if (imgList.length > maxFiles) {
+              Toast.show(`最多可上传${maxFiles}张图片`, {
+                position: Toast.positions.CENTER
+              });
+              return;
             }
-          });
-          const res = await uploadImages({ files });
-        }}>
-          <Text style={styles.loginText}>确定发布</Text>
-        </TouchableOpacity>
-      </View>
+            const files = imgList.map((item, index) => {
+              const imgType = item.mime && item.mime.split('/')[1];
+              return {
+                path: item.path,
+                filename: 'sl204984_' + index + '.' + (imgType || 'jpeg') // 命名规则：订单号 + 索引 + 文件类型
+              }
+            });
+            const res = await uploadImages({ files });
+          }}>
+            <Text style={styles.loginText}>确定发布</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     )
   }
 }
