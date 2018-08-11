1a277dbb719c5767211e9423b14bea4780ac7715
sl204984 <17798520003@163.com>
2018-08-12 06:19:41 +0800

    版本

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,15 +1,19 @@
 let CONFIG;
+const version = '0.0.1';
+const HTTP_TIME_OUT = 5;
 if (__DEV__) {
   CONFIG = {
     HOST: "http://localhost:3000/",
     IMG_HOST: "http://localhost:3000/",
-    HTTP_TIME_OUT: 5,
+    HTTP_TIME_OUT,
+    version
   }
 } else {
   CONFIG = {
     HOST: "http://47.99.72.101/",
     IMG_HOST: "http://47.99.72.101/",
-    HTTP_TIME_OUT: 5,
+    HTTP_TIME_OUT,
+    version
   }
 }
 
